e03e00ed8f167f45af5b12310ed8c09e1d8ce65d
rohitsinghind <rohitsinghind22@mail.com>
2022-11-21 12:57:06 +0530

    rrgfnbguy

--- client/src/screens/applicationForm/components/privacyPolicyPopup/index.jsx
+++ client/src/screens/applicationForm/components/privacyPolicyPopup/index.jsx
@@ -29,16 +29,16 @@ export default function PrivacyPolicyPopup(props) {
       >
         <DialogTitle>Privacy Policy</DialogTitle>
         <DialogContent>
-          <DialogContentText>
+          <DialogContentText sx={{mb:2}}>
           Thank you for visiting the JUSCO website. The Company is sensitive to privacy issues on the internet. Visitors are guaranteed privacy and any information collected is kept private and never shared with other organizations. It is used only by the Company to administer your request, if any. At times when we need information from you, we will try (but are not obligated) to let you know how this information is going to be used.
           </DialogContentText>
-          <DialogContentText>
+          <DialogContentText sx={{mb:2}}>
           Your IP (Internet Protocol) address may be used to gather broad demographic information to measure the number of visits, average time spent on the site, pages viewed, etc. This helps us maintain and administer the website better and also to improve the content of the website to address the more relevant stakeholder issues.
           </DialogContentText>
-          <DialogContentText>
+          <DialogContentText sx={{mb:2}}>
           All rights are reserved by JUSCO Ltd. The content, code and applications contained on this website, under the domain www.juscoltd.com are copyright protected. Site visitors may not reproduce, copy, or redistribute content or code in any form without written permission from the Company.
           </DialogContentText>
-          <DialogContentText>
+          <DialogContentText sx={{mb:2}}>
           This site contains links to sites outside www.juscoltd.com. JUSCO is not responsible for the privacy practices, or the content, of such other websites.
           </DialogContentText>
           <DialogContentText>
